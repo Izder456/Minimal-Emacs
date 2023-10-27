34cc322c66e69db2ee778fe5aabce0ecd85ba953
izder456 <izder456@disroot.org>
2023-10-27 03:44:14 -0500

    fix linums

--- init.el
+++ init.el
@@ -10,3 +10,15 @@
 (package-initialize)
 (load-file "~/.emacs.d/packages.el")
 (load-file "~/.emacs.d/config.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dap-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

9a9fb9bf240fed74e76965794254440c8d6ab251
izder456 <izder456@disroot.org>
2024-02-17 15:55:47 -0600

    use astyle

--- init.el
+++ init.el
@@ -431,14 +431,10 @@
   :hook ((markdown-mode . visual-line-mode)))
 
 ;; format
-(use-package format-all
-  :commands format-all-mode
-  :hook (prog-mode . format-all-mode)
-  :config
-  (setq-default format-all-formatters
-                '(("C" (astyle "--mode=c"))
-                  ("Java" (astyle "--mode=java"))
-                  ("Shell" (shfmt "-i" "4" "-ci")))))
+(use-package astyle
+  :ensure t
+  :when (executable-find "astyle")
+  :hook (c-mode-common . astyle-on-save-mode))
 
 ;; Elixir
 (defun elixir-inf-helper (lis)
@@ -567,10 +563,6 @@
 ;; org
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(org-level-1 ((t (:inherit outline-1 :height 1.1))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
@@ -730,10 +722,3 @@ If the new path's directories does not exist, create them."
 ;; i want line numbers when i program !!
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (add-hook 'text-mode-hook 'visual-line-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(forth-mode yaml-mode which-key vterm-toggle undo-tree toc-org sly rust-mode raku-mode rainbow-mode rainbow-delimiters org-superstar org-auto-tangle org-appear nyan-mode neotree markdown-mode json-mode ivy-prescient inf-elixir hy-mode hl-todo general geiser-chicken format-all flycheck-rust flycheck-raku flycheck-projectile flycheck-ocaml flycheck-elixir flycheck-clojure evil-collection elixir-mode editorconfig doom-themes doom-modeline denote dashboard counsel company-prescient company-box beacon all-the-icons-ivy-rich all-the-icons-dired)))
